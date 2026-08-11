0b17db34f4efd5fd3c8c93457c792c8baae5263e
vanshpunmiya318 <vanshpunmiya318@gmail.com>
2026-08-11 08:51:51 +0100

    Update Q1.sql

--- Sql/Q1.sql
+++ Sql/Q1.sql
@@ -1,1 +1,11 @@
+-- Q1: Department Attrition Rate
+-- Business Question: Which department has the highest employee attrition?
+
+SELECT Department AS "Department",
+       COUNT(*) AS "Total Employees",
+       SUM(CASE WHEN Attrition = 'Yes' THEN 1 ELSE 0 END) AS "Employees Left",
+       ROUND(SUM(CASE WHEN Attrition = 'Yes' THEN 1 ELSE 0 END) * 100 / COUNT(*), 2) || '%' AS "Attrition Rate"
+FROM HR_ANALYTICS
+GROUP BY Department
+ORDER BY 4 DESC;
 
